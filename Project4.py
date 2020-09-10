d8e774fbb955895abdf8e11c0089d1c68ad3bc29
Nik Paulic <62592843+Nik620@users.noreply.github.com>
2020-09-10 17:19:27 +0900

    Create Project4.py

---
+++ Project4.py
@@ -0,0 +1,23 @@
+# [ ] create, call and test the str_analysis() function  
+# then PASTE THIS CODE into edX  
+def str_analysis(string):
+    if string.isdigit():
+        string = int(string)
+        if string > 99:
+            return print(string,"is a pretty big number")
+        else:
+            return print(string,"is a smaller number than expected")
+    elif string.isalpha():
+        return print("\""+string+"\" is all alphabetical characters!")
+    else:
+        return print("Thats neither a digit nor an alpha")
+
+msg = input("enter a word or integer: ")
+
+while True:
+    if msg:
+        break
+    else:
+        msg = input("enter a word or integer: ")
+        
+str_analysis(msg)
