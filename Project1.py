5216cc158a925945861a6c6dbd16e47bfbef170c
Nik Paulic <62592843+Nik620@users.noreply.github.com>
2020-09-10 17:15:54 +0900

    Create Project1.py

---
+++ Project1.py
@@ -0,0 +1,17 @@
+# Create Allergy check code
+# then PASTE THIS CODE into edX
+
+# [ ] get input for input_test variable
+input_test = '"'+ input("Please enter things eaten in the last 24 hours: ").lower()+'"'
+
+# [ ] print "True" message if "dairy" is in the input or False message if not
+print("It is","dairy" in input_test,"that" ,input_test, 'contains "dairy".')
+
+# [ ] print True message if "nuts" is in the input or False if not
+print("It is","nuts" in input_test,"that" ,input_test, 'contains "nuts".')
+
+# [ ] Challenge: Check if "seafood" is in the input - print message
+print("It is","seafood" in input_test,"that" ,input_test, 'contains "seafood".')
+
+# [ ] Challenge: Check if "chocolate" is in the input - print message
+print("It is","chocolate" in input_test,"that" ,input_test, 'contains "chocolate".')
